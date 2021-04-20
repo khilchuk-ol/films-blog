71073aa23846851fa92187a10e3873c1ae6a5b53
Olena Khilchuk <olena.khilchuk@gmail.com>
2021-04-20 21:08:30 +0300

    model updated

--- app/models/post.model.js
+++ app/models/post.model.js
@@ -1,20 +1,29 @@
 export default mongoose => {
     let schema = mongoose.Schema(
         {
-            title: String,
+            title: { type: String, required: true },
             description: String,
             text: String,
-            comments: []
+            comments: [{
+              date: Date,
+              username: String,
+              text: String,
+              rating : {type: Number, min: 0, max: 10, require: true}
+            }]
         },
         { timestamps: true }
       );
     
-      schema.method("toJSON", function() {
-        const { __v, _id, ...object } = this.toObject();
-        object.id = _id;
-        return object;
-      });
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+
+    schema.virtual('rating').get(function() {
+      return this.comments ? this.comments.map(c => c.rating).reduce((a, b) => a + b, 0) / this.comments.length : 0;
+    });  
     
-      const Post = mongoose.model("post", schema);
-      return Post;
+    const Post = mongoose.model("post", schema);
+    return Post;
   };
